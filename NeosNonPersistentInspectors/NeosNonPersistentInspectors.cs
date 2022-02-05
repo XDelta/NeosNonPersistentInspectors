f19fd996ac01211a1a07b92ab7489786020830d1
Delta <delta@deltawolf.tk>
2022-02-04 23:20:57 -0800

    Initial commit

---
+++ NeosNonPersistentInspectors/NeosNonPersistentInspectors.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+
+namespace NeosNonPersistentInspectors {
+	public class NeosNonPersistentInspectors : NeosMod {
+		public override string Name => "NeosNonPersistentInspectors";
+		public override string Author => "Delta";
+		public override string Version => "1.0.0";
+		public override string Link => "https://github.com/XDelta/NeosNonPersistentInspectors/";
+
+		public override void OnEngineInit() {
+			Harmony harmony = new Harmony("tk.deltawolf.NeosNonPersistentInspectors");
+			harmony.PatchAll();
+			Msg("Inspectors NonPersistent'd!");
+		}
+
+		[HarmonyPatch(typeof(SceneInspector), "OnAttach")]
+		static class SceneInspector_OnAttach_Patch {
+			static void Postfix(SceneInspector __instance) {
+				__instance.Slot.PersistentSelf = false;
+			}
+		}
+	}
+}
